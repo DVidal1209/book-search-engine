7c8d8a4dd41ad63be6965fec5b528590390b6d19
DVidal1209 <daniel.vidal17@yahoo.com>
2023-01-25 11:19:32 -0500

    Completed server.js file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
