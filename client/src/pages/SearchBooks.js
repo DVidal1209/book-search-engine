f3c3b1bd1c8190df490330ad2946bfdc20d6d4b9
DVidal1209 <daniel.vidal17@yahoo.com>
2023-01-25 20:43:55 -0500

    Added authMiddleware

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -74,11 +74,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({ bookToSave });
+      const data = await saveBook({
+        variables: { ...bookToSave }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
